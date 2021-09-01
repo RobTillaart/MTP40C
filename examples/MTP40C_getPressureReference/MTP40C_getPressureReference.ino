00e091962c821d71039bd364d4fed227df8018c3
Rob Tillaart <rob.tillaart@gmail.com>
2021-09-01 13:58:11 +0200

    Develop (#3)
    * version 0.2.0
    * add MTP40D derived class
    * many smaller / larger fixes
    * add experimental examples - not tested
    * add suppress Error + lastError()

--- examples/MTP40C_getPressureReference/MTP40C_getPressureReference.ino
+++ examples/MTP40C_getPressureReference/MTP40C_getPressureReference.ino
@@ -1,19 +1,22 @@
 //
-//    FILE: MTP40C_getCO2.ino
+//    FILE: MTP40C_getPressureReference.ino
 //  AUTHOR: Rob Tillaart
 // VERSION: 0.1.0
 // PURPOSE: demo of MTP40C library
 //    DATE: 2021-08-21
 //     URL: https://github.com/RobTillaart/MTP40C
 //
-// any board that support two or more hardware serial ports 
+// any board that support two or more hardware serial ports
 // Serial and Serial1, e.g. for MEGA, LEONARDO, MICRO, ESP32,ESP8266
 // Uno, Nano or Mini will fail to compile.
 
 
 #include "MTP40C.h"
+#include "SoftwareSerial.h"
 
-MTP40C mtp(&Serial1);
+SoftwareSerial sws(6, 7);
+MTP40C mtp(&sws);
+// MTP40C mtp(&Serial1);
 
 int lines = 10;
 
@@ -22,11 +25,11 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println(__FILE__);
-  Serial.print("MTP40C_LIB_VERSION:\t");
-  Serial.println(MTP40C_LIB_VERSION);
+  Serial.print("MTP40_LIB_VERSION:\t");
+  Serial.println(MTP40_LIB_VERSION);
 
-  Serial1.begin(19200);
-  if (mtp.begin() == false)
+  sws.begin(19200);
+  if (mtp.begin(0x64) == false)
   {
     Serial.println("Could not connect!");
     while(1);
@@ -46,7 +49,7 @@ void loop()
   {
     Serial.print(millis());
     Serial.print("\t");
-    Serial.print(mtp.getAirPressure(), 1);
+    Serial.print(mtp.getAirPressureReference(), 1);
     Serial.println();
     lines++;
   }
